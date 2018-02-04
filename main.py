541bd2442cdc37a861cf53e4b04969d3c6c961ce
Brian Ishii <brianlishii@gmail.com>
2018-02-03 23:07:43 -0800

    Attempted to change to a Person entity instead of ingredient entity

--- main.py
+++ main.py
@@ -19,20 +19,23 @@ def homepage():
 
     # Use the Cloud Datastore client to fetch information from Datastore about
     # each photo.
-    query = datastore_client.query(kind='Photos')
-    image_entities = list(query.fetch())    
+    #query = datastore_client.query(kind='Photos')
+    #image_entities = list(query.fetch())    
 
     # Return a Jinja2 HTML template.
-    return render_template('homepage.html', image_entities=image_entities)
+    return render_template('homepage.html')
 
 @app.route('/upload_photo', methods=['GET', 'POST'])
 def upload_photo():
+
     # Create a Cloud Storage client.
     storage_client = storage.Client()
 
     # Get the Cloud Storage bucket that the file will be uploaded to.
     bucket = storage_client.get_bucket(os.environ.get('CLOUD_STORAGE_BUCKET'))
 
+
+    kind = 'Photo' 
     # Create a new blob and upload the file's content to Cloud Storage.
     photo = request.files['file']
     blob = bucket.blob(photo.filename)
@@ -52,14 +55,10 @@ def upload_photo():
         'image': {'source': {'image_uri': source_uri}},
     })
     labels = response.label_annotations
-    faces = response.face_annotations
     web_entities = response.web_detection.web_entities
 
     # Create a Cloud Datastore client
     datastore_client = datastore.Client()
-
-    # The kind for the new entity
-    kind = 'Photos'
 
     # The name/ID for the new entity
     name = blob.name
@@ -80,9 +79,25 @@ def upload_photo():
     # Save the new entity to Datastore
     datastore_client.put(entity)
 
-    # Redirect to the home page.
-    return render_template('homepage.html', labels=labels, faces=faces, web_entities=web_entities, public_url=image_public_url)
+    # TODO: Get user information through a form 
+    user_entity = create_user(username)
 
+    labels = check_fruit(labels)
+    # Redirect to the home page.
+    ingredients = print_ingredients()
+    return render_template('homepage.html', labels=labels, public_url=image_public_url, ingredients=ingredients)
+
+
+def create_user(username):
+    datastore_client = datastore.Client()
+    entity_kind = 'Person'
+    key = datastore_client.key(entity_kind, username)
+    entity = datastore.Entity(key)
+    entity.update({
+        'name':username
+        'ingredients':[]
+        })
+    return entity 
 
 @app.errorhandler(500)
 def server_error(e):
@@ -90,6 +105,61 @@ def server_error(e):
     An internal error occurred: <pre>{}</pre>
     See logs for full stacktrace.
     """.format(e), 500
+
+def check_fruit(labels, user):
+    fruit_labels = []
+    fruits = ["apple","apricot","avocado","banana","bell pepper",
+        "bilberry","blackberry","blackcurrant","blood orange","blueberry",
+        "boysenberry","breadfruit","canary melon","cantaloupe","cherimoya",
+        "cherry","chili pepper","clementine","cloudberry","coconut","cranberry",
+        "cucumber","currant","damson","date","dragonfruit","durian","eggplant",
+        "elderberry","feijoa","fig","goji berry","gooseberry","grape",
+        "grapefruit","guava","honeydew","huckleberry","jackfruit","jambul",
+        "jujube","kiwi fruit","kumquat","lemon","lime","loquat","lychee",
+        "mandarine","mango","mulberry","nectarine","nut","olive","orange",
+        "pamelo","papaya","passionfruit","peach","pear","persimmon","physalis",
+        "pineapple","plum","pomegranate","pomelo","purple mangosteen","quince","raisin",
+        "rambutan","raspberry","redcurrant","rock melon","salal berry","satsuma",
+        "star fruit","strawberry","tamarillo","tangerine","tomato","ugli fruit",
+        "watermelon"]
+    for label in labels:
+        for fruit in fruits:
+            if label.description == fruit:
+                update_ingredients(user, label.description)
+                print("new ingredient")
+                print(label.description)
+                fruit_labels.append(label)
+    return fruit_labels
+
+def update_ingredients(user, label):
+    user.update({
+        ingredients.append(label)})
+    datastore_client.put(user)
+
+"""
+def check_ingredients(label):
+    datastore_client = datastore.Client("foodies-194120")
+    query = datastore_client.query(kind='Ingredient')
+    ingredients = list(query.fetch())
+    for i in ingredients:
+        if (i.key.name == label.description):
+            print("same")
+            temp = datastore_client.get(i.key.name)
+            temp['count'] = temp['count'] + 1
+            datastore_client.put(temp)
+            return True
+    return False
+"""
+
+def print_ingredients():
+    ingred_list = []
+    datastore_client = datastore.Client("foodies-194120")
+    query = datastore_client.query(kind='Person')
+    user = list(query.fetch())[0]
+    for i in user.key.ingredients:
+        print(i)
+        ingred_list.append(i)
+    return ingred_list
 
 
 if __name__ == '__main__':
